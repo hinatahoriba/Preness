bb092646adddc182e9e4ab3765e98de1d6eed7a8
hinatahoriba <c0a2212903@edu.teu.ac.jp>
2026-02-09 17:07:47 +0900

    add: デザインをシンプル整えた

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def bootstrap_flash_class(level)
+    case level.to_sym
+    when :notice, :success
+      "alert alert-success"
+    when :alert, :error
+      "alert alert-danger"
+    when :warning
+      "alert alert-warning"
+    when :info
+      "alert alert-info"
+    else
+      "alert alert-secondary"
+    end
+  end
 end
