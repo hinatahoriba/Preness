99dd343649a9ef5f247e3a2d302e69863c7a994d
hinatahoriba <c0a2212903@edu.teu.ac.jp>
2026-03-19 15:40:55 +0900

    add: 採点画面にタグを追加した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def question_tag_label(tag)
+    return nil if tag.blank?
+
+    labels = {
+      'shortConv' => '短い会話',
+      'longConv'  => '長い会話',
+      'talk'      => 'トーク',
+      'sentenceStruct'  => '文構造',
+      'verbForm'        => '動詞の形',
+      'modifierConnect' => '修飾語・接続語',
+      'nounPronoun'     => '名詞・代名詞',
+      'vocab'     => '語彙',
+      'inference' => '推論',
+      'fact'      => '事実'
+    }
+    labels[tag] || tag
+  end
 end
