eb19b5464c25fe23ed27743c7a1002c73f37723c
hinatahoriba <c0a2212903@edu.teu.ac.jp>
2026-05-22 13:37:20 +0900

    fix: 実力試験がない時にバグらないようにした

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -3,7 +3,7 @@ class MypagesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    redirect_to new_initial_setting_path unless current_user.user_profile.present?
+    redirect_to new_initial_setting_path and return unless current_user.user_profile.present?
     @diagnostic = Diagnostic.first
   end
 end
